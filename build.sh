110b1a30aac54bfb9cda8091910f173c4e54ced8
Dhamodharan9787 <g.dhamu231@gmail.com>
2025-04-20 13:22:37 +0530

    build.sh docker repo updated

--- build.sh
+++ build.sh
@@ -5,10 +5,10 @@ echo "Current Branch: $BRANCH_NAME"
 if [ "$BRANCH_NAME" == "dev" ]; then
     docker build -t dhamu231/guvi_dev .
     #echo "Dhamu@123" | docker login -u "dhamu231" --password-stdin
-    docker push dhamu231/dev-react-app:latest
+    docker push dhamu231/guvi_dev:latest
 
 elif [ "$BRANCH_NAME" == "master" ]; then
     docker build -t dhamu231/guvi_prod .
     #echo "Dhamu@123" | docker login -u "dhamu231" --password-stdin
-    docker push dhamu231/prod-react-app:latest
+    docker push dhamu231/guvi_prod:latest
 fi
