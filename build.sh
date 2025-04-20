8aaeff0a689a6bf78e41cc76afc80e20eb6d6566
Dhamodharan9787 <g.dhamu231@gmail.com>
2025-04-20 13:07:46 +0530

    build.sh updated

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-# Variables
-IMAGE_NAME="dhamu231/react-app"
-DEV_TAG="dev"
-PROD_TAG="prod"
+echo "Current Branch: $BRANCH_NAME"
 
-docker login || exit 1
-docker build -t $IMAGE_NAME:$DEV_TAG .
-docker push $IMAGE_NAME:$DEV_TAG
-echo "Push completed"
+if [ "$BRANCH_NAME" == "dev" ]; then
+    docker build -t dhamu231/dev-react-app .
+    #echo "Dhamu@123" | docker login -u "dhamu231" --password-stdin
+    docker push dhamu231/dev-react-app:latest
+
+elif [ "$BRANCH_NAME" == "master" ]; then
+    docker build -t dhamu231/prod-react-app .
+    #echo "Dhamu@123" | docker login -u "dhamu231" --password-stdin
+    docker push dhamu231/prod-react-app:latest
+fi
