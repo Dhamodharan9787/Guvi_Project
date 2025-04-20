0e9afe8a57cf384702ce0ea0073631bdc57a0055
Dhamodharan9787 <g.dhamu231@gmail.com>
2025-04-20 13:41:36 +0530

    deploy.sh updated

--- deploy.sh
+++ deploy.sh
@@ -3,13 +3,16 @@
 # Variables
 echo "Deployment Branch:$BRANCH_NAME"
 #IMAGE_NAME="dhamu231/react-app"
+
+if  ["$BRANCH_NAME" == "dev"]; then
 docker stop react-app || true
 docker rm react-app || true
-if  ["$BRANCH_NAME" == "dev"]; then
 docker pull dhamu231/guvi_dev:latest
-docker run -d --name react-app -p 80:80 dhamu231/guvi_dev:latest
+docker run -d -p 80:80 --name react-app dhamu231/guvi_dev:latest
 elif  ["$BRANCH_NAME" == "master"]; then
+docker stop react-app || true
+docker rm react-app || true
 docker pull dhamu231/guvi_prod:latest
-docker run -d --name react-app -p 80:80 dhamu231/guvi_prod:latest
+docker run -d -p 80:80 --name react-app dhamu231/guvi_prod:latest
 fi
 echo "Deployment completed."
